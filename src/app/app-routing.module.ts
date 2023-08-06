c4943e8b406dfcee00701b18a27eb588438dc1d3
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-08-06 18:23:06 +0300

    Order component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { OrderComponent } from './pages/order/order.component';
 
 const routes: Routes = [
   {
@@ -12,7 +13,11 @@ const routes: Routes = [
   {
     path:'about',
     component:AboutComponent,
-  }
+  },
+  {
+    path:'order',
+    component:OrderComponent,
+  },
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { OrderComponent } from './order/order.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    OrderComponent
   ],
   imports: [
     CommonModule
@@ -16,6 +18,7 @@ import { AboutComponent } from './about/about.component';
   exports:[
     HomeComponent,
     AboutComponent,
+    OrderComponent,
   ]
 })
 export class PagesModule { }
